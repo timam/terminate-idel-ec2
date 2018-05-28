1acb7e95096280fd116e0a1fbcbb06c1d41e13d0
Timam <me@timam.io>
2018-05-29 01:07:29 +0600

    added history writing capability

--- mon.py
+++ mon.py
@@ -1,8 +1,14 @@
 import os
+from time import sleep
 
-# Get CPU Usage
-top = list(os.popen(''' top -b -d1 -n2 | grep -i "Cpu(s)" | tail -1  | awk '{ print $2 }' '''))
-percentage = float((top[0]).replace("\n", ""))
+while True:
+    # Get CPU Usage
+    top = list(os.popen(''' top -b -d1 -n2 | grep -i "Cpu(s)" | tail -1  | awk '{ print $2 }' '''))
+    percentage = (top[0]).replace("\n", "")
 
+    # Write percentage in a history file
+    with open("history.txt", "a") as file:
+        file.write(percentage + "\n")
 
+    sleep(60)
 
