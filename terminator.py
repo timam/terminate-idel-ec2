cc7a542b5baf11bb8ad0b65b0459e7dde1f54293
Timam <me@timam.io>
2018-05-29 04:54:52 +0600

    stoping instance, fixed filename, boto3 added

--- terminator.py
+++ terminator.py
@@ -1,18 +1,37 @@
+#!/usr/bin/python3
+
+import boto3
+from secrets import *
+
+client = boto3.client(
+    'ec2',
+    region_name=gimme_aws_region(),
+    aws_access_key_id=gimme_aws_access_id(),
+    aws_secret_access_key=gimme_aws_secret_access_key()
+)
 # Reading contents of history
-file_handle = open('history.txt', "r")
+file_handle = open('usage.txt', "r")
 raw_list = file_handle.readlines()
 file_handle.close()
 
 # Manipulating usage list
 cpu_usage_str_list = [item.replace('\n', '') for item in raw_list]
-cpu_usage_list = list(map(float,cpu_usage_str_list))
+cpu_usage_list = list(map(float, cpu_usage_str_list))
 
 # Checking, if cpu is idle or not
 if len(cpu_usage_list) < 15:
     print("Not enough Data")
 
 else:
-    if all(i < 20.0 for i in cpu_usage_list):
+    if all(i < 20.0 for i in cpu_usage_list[-15:]):
         print("Idle CPU")
+
+        response = client.stop_instances(
+            InstanceIds=[
+                'i-0781f9e781c15b0c4',
+            ],
+        )
+
+
     else:
         print("CPU is in use")
