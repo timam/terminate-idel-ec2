9d8dad470f71aeb29f333339f4ffd8bcefb4e073
Timam <me@timam.io>
2018-05-29 00:30:57 +0600

    cleaner cpu percentage

--- mon.py
+++ mon.py
@@ -1,9 +1,8 @@
 import os
 
+# Get CPU Usage
+top = list(os.popen(''' top -b -d1 -n2 | grep -i "Cpu(s)" | tail -1  | awk '{ print $2 }' '''))
+percentage = float((top[0]).replace("\n", ""))
 
-percentage = list(os.popen(''' top -b -d1 -n2 | grep -i "Cpu(s)" | tail -1  | cut -d ' ' -f2 '''))
-temp = percentage[0]
-var = temp[:-1]
-print(var)
 
 
