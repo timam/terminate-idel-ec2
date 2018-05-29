eab6a04f86db0fb122347ae1e253d6b6a83a0d08
Timam <me@timam.io>
2018-05-30 01:39:45 +0600

    termination message working

--- terminator.py
+++ terminator.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-import os
 import signal
 import boto3
 from secrets import *
+from time import sleep
 
 client = boto3.client(
     'ec2',
@@ -11,6 +11,7 @@ client = boto3.client(
     aws_access_key_id = gimme_aws_access_id(),
     aws_secret_access_key= gimme_aws_secret_access_key()
 )
+
 # Reading contents of history
 file_handle = open('/var/opt/usage.log', "r")
 raw_list = file_handle.readlines()
@@ -27,15 +28,19 @@ if len(cpu_usage_list) < 15:
 else:
     if all(i < 20.0 for i in cpu_usage_list[-15:]):
 
-        with open("/var/opt/usage.log", "a") as file:
-            file.write("CPU IDLE for last 15 mins, Terminating.....")
-
         # Killing monitor.py
         monitor_pid = list(os.popen(''' ps -aux | grep -v grep | grep monitor.py | awk '{ print $2 }' '''))
         pid = (monitor_pid[0]).replace("\n", "")
         os.kill(int(pid), signal.SIGTERM)
         print("Killing pid" + pid)
 
+        #Writing termination message
+        with open("/var/opt/usage.log", "a") as file:
+            file.write("CPU IDLE for last 15 mins, Terminating.....")
+
+        sleep(60)
+
+        #Terminating Instance
         response = client.terminate_instances(
             InstanceIds =  gimme_instence_id()
         )
